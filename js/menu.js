785aad21a6a6a92e4b6e38f13fe4be72e3ebf59e
rupali18082003 <missrupalithenaivegirl@gmail.com>
2021-12-08 22:37:41 +0530

    adjustments amd cart added

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
-const dishes = document.querySelector('.dishes');
+const dishes = document.querySelector('#dishes');
 const searchFoods = document.querySelector('#searchFoods');
+const Footer = document.querySelector('#footer');
 
 //food menu object
 let searchMenu = [{
@@ -31,22 +32,35 @@ let searchMenu = [{
         img: 'images/f2.jpg',
         food: 'Juice',
         desc: 'A short description'
+    },
+    {
+        price: 99,
+        img: 'images/f2.jpg',
+        food: 'Pizza',
+        desc: 'Yummy Pizza'
     }
 ];
 searchMenu = searchMenu.map((item, index) => ({...item, id: index })) //added an id generator
 
 //generating markup
-const generateMarkup = (menu) => {
-    return `
-        <div class="dish">
-            <div class="price">$${menu.price}</div>
-            <img src="${menu.img}">
+const generateMarkupCard = (menu) => {
+   return `
+        <div class="col-md-6 col-lg-3">
+            <div class="card item" >
+                <div class="card-body text-center"> 
+                    <h4 class="price">$${menu.price}</h4>
+                    <img src="${menu.img}" class="rounded-circle my-3 w-50">
 
-            <div class="desc">
-                <h3 class="title heading">${menu.food}</h3>
-                <p class="para">${menu.desc}</p>
+                    <h3 class="card-title title">${menu.food}</h3>
+                    <p class="lead card-text text-muted">
+                       ${menu.desc} 
+                    </p>
+
+                    <input type="text" class="qtty fontAwesome border-0 border-bottom mb-3 text-center form-control" name="qtty" placeholder="&#xf500; Enter Quantity" style="font-family: Arial, 'Font Awesome 5 Pro'" required>
+
+                    <button class="btnOrd" data-fp="${menu.price}" data-fn="${menu.food}">Order Now!</button>
+                </div>
             </div>
-            <button class="btnOrd m-3"><a href="">Order now!</a></button>
         </div>
     `;
 }
@@ -54,7 +68,7 @@ const generateMarkup = (menu) => {
 //rendering results
 const render = (menu) => {
     dishes.innerHTML = '';
-    menu.map(dish => dishes.insertAdjacentHTML('beforeend', generateMarkup(dish)))
+    menu.map(dish => dishes.insertAdjacentHTML('beforeend',  generateMarkupCard(dish)))
 }
 
 //default
@@ -63,15 +77,96 @@ render(searchMenu);
 //searching
 searchFoods.addEventListener('input', () => {
     const input = (searchFoods.value).toLowerCase();
-    const searchFood = searchMenu.filter(menu => menu.food.includes(input));
-
-    if (searchFood.length === 0) {
+    const searchFood = searchMenu.filter(menu => menu.food.toLowerCase().includes(input));
+    Footer.style.display = 'block';
+    if(searchFood.length === 0){
+        Footer.style.display = 'none';
         return dishes.innerHTML = `
             <h1 class='m-auto text-center'>No Results</h1>
             `
-    } else if (input === '') {
+    }else if(input === ''){
         render(searchMenu);
-    } else {
+    }else{
         render(searchFood);
     }
+})
+
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@___BILLING___@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+const orderBtns = document.querySelectorAll('.btnOrd');
+// const qtty = document.querySelector('.qtty');
+const bill = document.querySelector('.bill');
+const billText = document.querySelector('#bill');
+
+const prices = [];
+const foods = [];
+
+orderBtns.forEach((order) => {
+    order.addEventListener('click', e => {
+        e.preventDefault();
+        const parent = e.target.closest('.card');
+        const qtty = parent.querySelector('.qtty');
+        const food = parent.querySelector('.title');
+        const priceOrg = parent.querySelector('.price').textContent;
+        const price = +(priceOrg.replace('$', ''))
+        // console.log(parent);
+
+        if(qtty.value == ''){
+            alert("Please enter the qtty");
+        }else{
+            // console.log((qtty.value) * price);
+             order.textContent = 'Ordered!';
+
+             prices.push((qtty.value) * price)
+             foods.push(food.textContent)
+             // console.log(foods)
+
+             parent.classList.add('selected');
+             qtty.value = '';
+        }
+
+        // const fp = +order.dataset.fp;
+        // const fn = order.dataset.fn;
+
+       
+    })
+})
+
+bill.addEventListener('click', () => {
+    const reducer = (pv, cv) => pv + cv;
+
+    if(prices.length === 0){
+        billText.textContent = "Please order something!"
+    }else{
+         billText.textContent = '$' + prices.reduce(reducer);
+    }
+   
+})
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@___CART___@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+const orders = document.querySelector('#Orders');
+const ordersList = document.querySelector('.orders-list');
+const cancel = document.querySelector('.cancel');
+
+const generateMarkupOrders = (name, price) => {
+    const markup = `
+        <li class="list-group-item d-flex justify-content-between align-items-start mb-2  border-left">
+          <div class="ms-3 me-auto">
+             <h4>${name}</h4>
+                <span class="text-center">$${price}</span>
+          </div>
+          <button class="btn btn-secondary border-0 bg-orange rounded text-light cancel">
+              Cancel
+          </button>
+      </li>
+    `;
+    ordersList.insertAdjacentHTML('afterbegin', markup);
+}
+
+
+orders.addEventListener('click', (e) => {
+    console.log(foods);
+   for(let i in prices){
+      generateMarkupOrders(foods[i], prices[i]);
+   }
 })
