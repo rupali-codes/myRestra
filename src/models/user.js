5c67738be9e32311660703a9f5d8e0c702f50ad4
rupali18082003 <missrupalithenaivegirl@gmail.com>
2022-01-28 10:46:18 +0530

    bug fix

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const validator = require('Validator')
+const validator = require('validator')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const userSchema = new mongoose.Schema({
