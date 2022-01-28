10f23d28414cf5c04c07261743eecdbc8650d263
rupali18082003 <missrupalithenaivegirl@gmail.com>
2022-01-28 14:15:11 +0530

    bug fix

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, 
 	{
-		useNewUrlParser: true
+		useNewUrlParser: true,
+		useUnifiedTopology: true
 	}).then(res => {
 		console.log("connection successful")
 	}).catch(err => {
